560255f6bd7a76716e3d64bc20f32955008c7f90
Artturi Lehtiö <artturi.lehtio@aalto.fi>
2011-07-28 15:30:30 +0300

    Now supports dragging the map to scroll it

--- application.py
+++ application.py
@@ -12,6 +12,33 @@ class MainView(QGraphicsView):
     def __init__(self, scene):
         super(MainView, self).__init__(scene)
 
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.dragPos = event.globalPos()
+            self.dragPossible = True
+            self.dragging = False
+        super(MainView, self).mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.setCursor(Qt.ArrowCursor)
+            self.dragPossible = False
+            self.dragging = False
+        super(MainView, self).mouseReleaseEvent(event)
+
+    def mouseMoveEvent(self, event):
+        if self.dragPossible:
+            self.setCursor(Qt.ClosedHandCursor)
+            if self.dragging:
+                dx, dy = (self.dragPos - event.globalPos()).toTuple()
+                self.dragPos = event.globalPos()
+                self.verticalScrollBar().setValue(self.verticalScrollBar().value() + dy)
+                self.horizontalScrollBar().setValue(self.horizontalScrollBar().value() + dx)
+                self.verticalScrollBar().triggerAction(QAbstractSlider.SliderMove)
+                self.horizontalScrollBar().triggerAction(QAbstractSlider.SliderMove)
+            elif (self.dragPos - event.globalPos()).manhattanLength() < QApplication.startDragDistance():
+                self.dragging = True
+                
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
@@ -41,6 +68,7 @@ class MainWindow(QMainWindow):
 
         self.setCentralWidget(self.mainView)
         self.mainView.show()
+        self.mainView.centerOn(self.mainScene.sceneRect().center())
 
 if __name__ == "__main__":
     app = QApplication([])
