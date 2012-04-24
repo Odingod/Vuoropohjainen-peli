51cf65df88f8406d91585c9d1b08373bd7ba8c62
Eetu Haavisto <eetu.haavisto@gmail.com>
2012-04-24 09:57:54 +0300

    Unit tests for Settlement

--- runTests.py
+++ runTests.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, sys
 from Map import *
 from Players import *
 from PySide.QtGui import QApplication
@@ -23,13 +23,40 @@ class TestPlayer(unittest.TestCase):
 	def tearDown(self):
 		pass
 
-		
-class DummyGame():
 
-	def __init__(self):
-		self.mode=None
-	
+#test created by Eetu Haavisto
+class TestSettlement(unittest.TestCase):
+    def setUp(self):
+        from Settlement import *
+        self.dg=DummyGame()
+        self.p1=HumanPlayer(self.dg)
+        self.settlement = Settlement(population=1500, owner=self.p1, map=DummyMap())
+    def test_population(self):
+        self.assertEqual(1500, self.settlement.population, "Population initialized incorrectly")
+        self.settlement.build("farm")
+        self.assertEqual(1600, self.settlement.population, "Building a farm doesn't increase population")
+    def test_recruitment(self):
+        self.assertFalse(self.settlement.do_recruit(DummyUnit, self.p1, DummyTile(), 10000), "It's possible to recruit a unit too expensive")
+        self.assertTrue(self.settlement.do_recruit(DummyUnit, self.p1, DummyTile(), 500), "Recruitment doesn't work")
+        self.assertEqual(500, self.p1.treasury, "Recruitment doesn't affect owner's treasury")
+    def tearDown(self):
+        pass
+class DummyGame():
+    def __init__(self):
+        self.mode=None
+    def nextPlayerAction(self):
+        pass
+class DummyUnit():
+    def __init__(self, owner, tile):
+        pass
+class DummyTile():
+    def addUnit(self, unit):
+        pass
+class DummyMap():
+    def __init__(self):
+        self.units = []
 
 if __name__ == '__main__':
-	suite = unittest.TestLoader().loadTestsFromTestCase(TestPlayer)
-	unittest.TextTestRunner(verbosity=2).run(suite)
+    suite = unittest.TestLoader().loadTestsFromModule(sys.modules[__name__])
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    
